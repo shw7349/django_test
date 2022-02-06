bb5a159429fa01507991b05cf1eaf20ecb7c996d
SongHyunwoo <shw7349@naver.com>
2022-02-07 06:25:37 +0900

    Python Django 2일차 2

--- add_two_numbers.js
+++ add_two_numbers.js
@@ -1,3 +1,7 @@
-function whatTimeIsIt(){
-    alert(new Date());
-}
+    function doSomething(){
+        let a = document.getElementById("inputA").value;
+        let b = document.getElementById("inputB").value;
+        document.getElementById("valueA").innerHTML = a;
+        document.getElementById("valueB").innerHTML = b;
+        document.getElementById("valueC").innerHTML = Number(a) + Number(b);
+    }
