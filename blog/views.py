2caeb8c01c71dcda243a965bba08d4c7a9e56846
SongHyunwoo <shw7349@naver.com>
2022-02-11 22:55:30 +0900

    python django 4일차

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 from .models import Post
 
 def index(request):
-    posts = Post.objects.all().order_by('pk')
+    posts = Post.objects.all().order_by('-pk')
 
 
     return render(
         request,
         'blog/index.html',
         {
-            'posts' : posts,
+            'posts': posts,
         }
     )
 
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(
+        request,
+        'blog/single_page.html',
+        {
+            'post' : post,
+        }
+    )
